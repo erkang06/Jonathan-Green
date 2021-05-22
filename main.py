779b561c2a0f66ef1c138de7a13bc0b35c7fcca7
erkang06 <84463577+erkang06@users.noreply.github.com>
2021-05-22 13:44:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ UClient = UrbanClient()
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('I have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
